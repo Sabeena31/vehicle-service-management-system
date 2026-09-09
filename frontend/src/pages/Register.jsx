8920ac85ce75144d4298cb1dd39873f33c9f446f
Sabeena Kachary <131359833+Sabeena31@users.noreply.github.com>
2026-09-09 15:34:20 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -25,7 +25,7 @@ export default function Register() {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch("https://vehicle-service-management-system-production-5cde.up.railway.app/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
