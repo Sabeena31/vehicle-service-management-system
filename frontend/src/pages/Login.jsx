90728dc4778202c4db9698abd953c2c682e3f447
Sabeena Kachary <131359833+Sabeena31@users.noreply.github.com>
2026-09-09 15:33:07 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -31,7 +31,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://vehicle-service-management-system-production-5cde.up.railway.app/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
